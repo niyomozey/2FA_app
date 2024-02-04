d73f09ef6ff3d520327f7e5d9881677066d92bdc
niyomozey <moiseniyonkuru1@gmail.com>
2024-02-04 18:17:36 +0200

    Add pages

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <H1>Home Page</H1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ frontend/src/pages/UserDashboard.js
@@ -0,0 +1,10 @@
+const UserDashboard = () => {
+
+    return (
+        <div>
+            <H1>User Dashboard</H1>
+        </div>
+    );
+}
+
+export default UserDashboard;
