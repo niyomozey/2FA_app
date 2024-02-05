4e33077134e3d50fffd82d42383cc8dbe8bb54da
niyomozey <moiseniyonkuru1@gmail.com>
2024-02-05 23:19:00 +0200

    add tailwind

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div >
+    <div className="text-[40px] h-screen w-full flex flex-col justify-center items-center">
       <h1>Home Page</h1>
     </div>
   );
--- frontend/src/pages/UserDashboard.js
+++ frontend/src/pages/UserDashboard.js
@@ -1,7 +1,6 @@
 const UserDashboard = () => {
-
     return (
-        <div>
+        <div className="text-[40px] h-screen w-full flex flex-col justify-center items-center">
             <h1>User Dashboard</h1>
         </div>
     );
