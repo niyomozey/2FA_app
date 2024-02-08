127da787989b836413dfabb1b74492d23494f071
niyomozey <moiseniyonkuru1@gmail.com>
2024-02-08 23:44:10 +0200

    refine signup css

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -2,7 +2,7 @@ const Signup = ()=>{
     return (
         <div className="h-screen w-full flex justify-center items-center">
             <div className="w-[40%] border-2 border-gray-300 rounded-lg">
-                <div className="w-full flex flex-col justify-center items-center">
+                <div className="w-full flex flex-col justify-center items-center pb-4">
                     <h1 className="text-center text-[30px]">Signup Page</h1>
                     <p className="text-center text-[20px]">Provide your credentials to Signup.</p>
                 </div>
@@ -11,9 +11,9 @@ const Signup = ()=>{
                     <input className="mt-4 w-[80%] border-2 border-gray-300" type="password" placeholder="Password" />
                     <button className="mt-4 w-[80%] border-2 bg-gray-300">Signup</button>
                 </div>
-                <div>
+                <div className="w-full flex flex-col justify-center items-center m-4">
                     <p>Already have an account?</p>
-                    <a href="/login">Login</a>
+                    <a className="text-blue-500" href="/login">Login</a>
                 </div>
             </div>
         </div>
